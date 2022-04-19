ed173b619a35f22ef53c7ebd287de85d7551a7c0
Даня <daniakutsev1488@gmail.com>
2022-04-19 15:58:20 +0300

    71

--- src/components/Profile/ ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,6 @@
 import s from './ProfileInfo.module.css'
 import Preloader from "../../common/Preloader/Preloader";
-
+import ProfileStatus from "./ProfileStatus"
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -8,16 +8,13 @@ const ProfileInfo = (props) => {
     }
     return (
         <div className={s.descriptionBlock}>
-            <div>
-                <img className={s.contentImg}
-                     src='https://wallpapershome.ru/images/wallpapers/les-3840x2160-5k-4k-8k-tuman-holmi-6505.jpg'
-                     alt=""/>
-            </div>
+
             <div>
                 <img src={props.profile.photos.large}/>
                 <p>{props.profile.aboutMe}</p>
                 ava+description
             </div>
+            <div><ProfileStatus status={"my new status"}/></div>
         </div>
     )
 
